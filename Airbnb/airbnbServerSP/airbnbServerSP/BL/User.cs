5f4e194288b9b39a0a5e8a826dbe33f51a84db78
AviaPerelman <154759994+AviaPerelman@users.noreply.github.com>
2024-02-28 18:33:54 +0200

    Add files via upload

---
+++ Airbnb/airbnbServerSP/airbnbServerSP/BL/User.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace HomeWork2.BL
+{
+    public class User
+    {
+        string firstName;
+        string lastName;
+        string email;
+        string password;
+
+        public User(string firstName, string lastName, string email, string password)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.password = password;
+        }
+
+        public User() { }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+
+        // insert user - registration
+        public int Insert()
+        {
+            DBservices dbs = new DBservices();
+
+            List<User> users = Read();
+
+            foreach (var user in users)
+            {
+                if(user.email == this.email) { return -1; }
+            }
+
+            return dbs.InsertUser(this);
+        }
+
+        // get all users
+        public List<User> Read()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.ReadUsers();
+        }
+
+        //update user
+
+        public User Update()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.UpdateUser(this);
+
+        }
+
+        // login user
+        public User Login(string email, string password)
+        {
+            DBservices dbs = new DBservices();
+            return dbs.LoginUser(email, password);
+               
+        }
+
+        //delete user
+        public int Delete(string email)
+        {
+            DBservices dbs = new DBservices();
+            return dbs.DeleteUserByEmail(email);
+        }
+        
+    }
+}
---
+++ Airbnb/airbnbServerSP/airbnbServerSP/Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HomeWork2.BL;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HomeWork2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            User user = new User(); 
+            return user.Read();
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        //POST 
+
+        [HttpPost]
+        public int Post([FromBody] User user)
+        {
+
+            return user.Insert();
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        [Route("login")]
+        public ActionResult Login(User user)
+        {
+
+            User authenticatedUser = user.Login(user.Email, user.Password);
+
+            if (authenticatedUser != null)
+            {
+                // Return the authenticated user
+                return Ok(authenticatedUser);
+            }
+            else
+            {
+                // Return 404 if user does not exist or credentials are invalid
+                return NotFound("User not found or invalid credentials");
+            }
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("update")]
+        public User Put([FromBody] User user)
+        {
+            return user.Update();
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("delete")]
+        public int Delete(string email)
+        {
+            User user = new User();
+            return user.Delete(email);
+        }
+    }
+}
